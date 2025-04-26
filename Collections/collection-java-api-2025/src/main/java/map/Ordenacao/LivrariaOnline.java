1b3e310e42a17a639624ad3fb6f57ea12e814426
Pedro Henrique Camargo <pedrohcamargon@gmail.com>
2025-04-26 01:39:06 -0300

    Resolvendo Ordenacao AgendaEventos e terminar LivrariaOnline

---
+++ Collections/collection-java-api-2025/src/main/java/map/Ordenacao/LivrariaOnline.java
@@ -0,0 +1,40 @@
+package main.java.map.Ordenacao;
+
+import java.util.*;
+
+public class LivrariaOnline {
+
+    private final Map<String, Livro> livros;
+
+    public LivrariaOnline() {
+        this.livros = new HashMap<>();
+    }
+
+    public void adicionarLivro(String link, Livro livro) {
+        livros.put(link, livro);
+    }
+
+    public void removerLivro(String titulo) {
+        List<String> chaveRemover = new ArrayList<>();
+        for (Map.Entry<String, Livro> entry : livros.entrySet()) {
+            if (entry.getValue().getTitulo().equalsIgnoreCase(titulo)) {
+                chaveRemover.add(entry.getKey());
+            }
+        }
+        for (String chave : chaveRemover) {
+            livros.remove(chave);
+        }
+    }
+
+    public Map<String, Livro> exibirLivrosOrdenadosPorPreco() {
+        List<Map.Entry<String, Livro>> livrosParaOrdenarPorPreco = new ArrayList<>(livros.entrySet());
+
+        Collections.sort(livrosParaOrdenarPorPreco, new ComparatorPorPreco());
+        Map<String, Livro> livrosOrdenadosPorPreco = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorPreco) {
+            livrosOrdenadosPorPreco.put(entry.getKey(), entry.getValue());
+        }
+        return livrosOrdenadosPorPreco;
+    }
+}
